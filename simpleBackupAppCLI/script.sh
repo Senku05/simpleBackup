dc6a8ad804b09717ecc45f8ad838c74e5356f5d3
Senku05 <lelemol05@gmail.com>
2022-12-17 16:47:51 +0100

    Added simple move files function

--- simpleBackupAppCLI/script.sh
+++ simpleBackupAppCLI/script.sh
@@ -4,3 +4,16 @@
 # Version: 0.1 Alpha
 # Description: this tool will be able to do some simple backup of your most important file where you want
 
+function moveFunction() {
+    cp -r $pathToSave/ $destinationPath/
+}
+
+echo "Hi! Welcome to this tool in V.0.1 Alpha!"
+
+echo "Write the absolute path of the MAIN folder that you want to save (ONLY LINUX): "
+read pathToSave
+
+echo "Write the absolute path where you want to store your backup (NOT REMOTE LOCATION) (ONLY LINUX): "
+read destinationPath
+
+moveFunction
